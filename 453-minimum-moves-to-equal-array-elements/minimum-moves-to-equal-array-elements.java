64958f939eec1666a1de851ab584dc197c6ef794
Arpit Sahu <arpitsahu4589@gmail.com>
2026-04-06 23:16:08 +0530

    Time: 2 ms (93.00%) | Memory: 47.5 MB (27.09%) - LeetSync

---
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        
+        for (int num : nums) {
+            sum += num;
+            min = Math.min(min, num);
+        }
+        
+        return sum - (min * nums.length);
+    }
+}
