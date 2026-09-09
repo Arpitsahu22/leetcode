9075b91b25aa17fd1af005d9df3a92f45515bf84
Arpit Sahu <arpitsahu4589@gmail.com>
2026-09-09 11:52:20 +0530

    Time: 5 ms (94.22%) | Memory: 140.6 MB (71.35%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -13,6 +13,7 @@ class Solution {
 }
 */
 // Memoization
+/*
 class Solution {
     public long mostPoints(int[][] questions) {
         long[] dp = new long[questions.length];
@@ -26,5 +27,23 @@ class Solution {
         long skip = solve(questions, i + 1, dp);
         dp[i] = Math.max(take, skip);
         return dp[i];
+    }
+}
+*/
+// Tabulation
+class Solution {
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            int next = i + questions[i][1] + 1;
+            long take = questions[i][0];
+            if (next < n) {
+                take += dp[next];
+            }
+            long skip = dp[i + 1];
+            dp[i] = Math.max(take, skip);
+        }
+        return dp[0];
     }
 }
