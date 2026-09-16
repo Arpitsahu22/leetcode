1200df74bb8c6ce4edf3e8debcd9436a5311532a
Arpit Sahu <arpitsahu4589@gmail.com>
2026-09-16 10:39:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (65.43%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,4 +1,19 @@
+// Recursion
+/*
+class Solution {
+    public int uniquePaths(int m, int n) {
+        return solve(0, 0, m, n);
+    }
+    public int solve(int r, int c, int m, int n) {
+        if (r == m - 1 || c == n - 1) return 1;
+        int down = solve(r + 1, c, m, n);
+        int right = solve(r, c + 1, m, n);
+        return down + right;
+    }
+}
+*/
 // Memoization 
+/*
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
@@ -14,5 +29,22 @@ class Solution {
         int down = solve(row + 1, col, m, n, dp);
         int right = solve(row, col + 1, m, n, dp);
         return dp[row][col] = down + right;
+    }
+}
+*/
+// Tabulation
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        dp[m - 1][n - 1] = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1) continue;
+                int down = dp[i][j+1] ;
+                int right = dp[i+1][j];
+                dp[i][j] = down + right;
+            }
+        }
+        return dp[0][0];
     }
 }
